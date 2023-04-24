19208b6997a5f425c4cc5f05e4f5c455ed4df9d1
Josué Lustosa <josuelustosa.job@gmail.com>
2023-04-24 14:11:59 -0400

    fix: finalizando a questao 5

--- atividades-avaliativas/atividade-02/Q5/questao5.js
+++ atividades-avaliativas/atividade-02/Q5/questao5.js
@@ -1,10 +1,24 @@
-var btn = document.querySelector(".btn");
-var img = document.querySelector(".js-hidden");
+var btn = document.querySelector('.btn');
+var img = document.querySelector('.js-hidden');
+var removeBtn = document.querySelector('.remove-btn');
 
-btn.addEventListener("click", function () {
-  img.setAttribute("class", "js-show");
+// escondendo a imagem ao iniciar a página
+removeBtn.setAttribute('hidden', ''); 
+
+// exibindo a imagem e o botão de Remover Imagem ao clicar no botão Visualizar Imagem
+btn.addEventListener('click', function () {
+  img.setAttribute('class', 'js-show');
+  removeBtn.removeAttribute('hidden');
 });
 
-img.addEventListener("click", function () {
-  img.setAttribute("class", "js-hidden");
+// removendo a imagem e o botão de Remover Imagem ao clicar na imagem
+img.addEventListener('click', function () {
+  img.setAttribute('class', 'js-hidden');
+  removeBtn.setAttribute('hidden', '');
+});
+
+// removendo a imagem e o botão de Remover Imagem ao clicar no botão de Remover Imagem
+removeBtn.addEventListener('click', function () {
+  img.setAttribute('class', 'js-hidden');
+  removeBtn.setAttribute('hidden', '');
 });
